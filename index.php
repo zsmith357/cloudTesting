8a077a94f5f1d01d1cee073451741eb8dccd384d
Zach Smith <96314181+zsmith357@users.noreply.github.com>
2022-05-27 13:11:09 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo " Today is " . date("Y/m/d") . "<br>";
+echo " The time is " . date("h:i:sa") . "<br>";
+echo " I am loaded from volume-2 " . "<br>";
+echo " You are directed to this server -> ",gethostname();
+?>
